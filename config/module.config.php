a727e5101d02c251f4e87da7000852cd3fced1bf
alex <ua.quetzalcoatl@gmail.com>
2013-07-26 13:42:48 +0300

    added listener class

--- config/module.config.php
+++ config/module.config.php
@@ -1,4 +1,12 @@
 <?php
 return array(
-    'expedia' => array()
+    'expedia' => array(
+        'api_key' => 'your_api_key',
+        'secret' => 'your_secret',
+        'cid' => '55505', // Beta Access
+        'minorRev' => 25,
+        'locale' => 'en_US',
+        'currencyCode' => 'USD',
+        'use_signature_authentication' => true, // use it when IP authentication is not possible
+    )
 );
---
+++ src/QtzExpedia/Listener/Auth/Signature.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace QtzExpedia\Listener\Auth;
+
+/**
+ * Description of Signature
+ *
+ * @author Alex Savchenko
+ */
+class Signature
+{
+    //put your code here
+}
