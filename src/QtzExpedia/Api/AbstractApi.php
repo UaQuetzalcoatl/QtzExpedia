973e8d261b38c4522e4f267b25daf4ada64829e0
alex <ua.quetzalcoatl@gmail.com>
2013-07-31 13:30:47 +0300

    Added abstract api classes

---
+++ src/QtzExpedia/Api/AbstractApi.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace QtzExpedia\Api;
+
+/**
+ * Description of AbstractApi
+ *
+ * @author Alex Savchenko
+ */
+abstract class AbstractApi implements ApiInterface
+{
+    public function get()
+    {}
+
+    public function post()
+    {}
+}
---
+++ src/QtzExpedia/Api/Hotels.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace QtzExpedia\Api;
+
+/**
+ * Description of Hotels
+ *
+ * @author Alex Savchenko
+ */
+class Hotels extends AbstractApi
+{
+    public function hotelList($options)
+    {}
+
+    public function rateRules()
+    {}
+
+    public function roomAvailability()
+    {}
+
+    public function roomImages()
+    {}
+
+    public function info($hotelId, $options)
+    {}
+}
